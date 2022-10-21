55b4a450babd36bce0fb4fe91f12bd3f3c8f445f
Alexis Gonzalez <alexis_gonzalez@hotmail.fr>
2022-10-21 10:54:12 +0200

    validations + add name  and description to menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
   has_many :orders_items
   belongs_to :meal
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
