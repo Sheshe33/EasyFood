428438eb0632e7bdb3dfe8e8fa90a041382a82f4
sherazade othman <othmansherazade@gmail.com>
2022-10-25 10:47:28 +0200

    crud order items

--- app/controllers/orders_items_controller.rb
+++ app/controllers/orders_items_controller.rb
@@ -1,2 +1,15 @@
 class OrdersItemsController < ApplicationController
+
+  #def index
+    #@orders_items = Order_item.all
+  #end
+
+  #def create
+    #@order_item = Order_item.find(params[:order_item_id])
+
+  #end
+
+  #def destroy
+
+  #end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# User.destroy_all
+# Category.destroy_all
+
+# for user
+
+# puts "Destroying users..."
+# puts "Creating users..."
+
+# User.create!(email: "lisa@gmail.com", password: "secret", role: "manager")
+# User.create!(email: "marco@gmail.com", password: "secret", role: "waiter")
+# User.create!(email: "tony@gmail.com", password: "secret", role: "cook")
+
+# puts "#{User.count} users created!"
+
+
+# for categories
+
+# puts "Destroying categories..."
+# puts "Creating categories..."
+
+# Category.create!(title: "drinks")
+# Category.create!(title: "meals")
+# Category.create!(title: "menus")
+
+# puts "#{Category.count} categories created!"
