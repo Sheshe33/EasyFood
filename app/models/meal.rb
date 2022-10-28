710f0778af2582f72faebcc15bed651f69173ba3
sherazade othman <othmansherazade@gmail.com>
2022-10-28 12:26:44 +0200

    seed

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ApplicationRecord
   has_many :orders_items
   belongs_to :category
-  has_one_attached :image_url
+
 
   validates :name, presence: true
   validates :description, presence: true
